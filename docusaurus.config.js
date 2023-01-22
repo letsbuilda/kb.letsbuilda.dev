a5da640eb7d72b12747790a15fca9b1cbbe9d631
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-01-22 01:10:54 -0600

    Highlight PowerShell syntax
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,6 +110,7 @@ const config = {
             prism: {
                 theme: lightCodeTheme,
                 darkTheme: darkCodeTheme,
+                additionalLanguages: ['powershell'],
             },
         }),
 };
